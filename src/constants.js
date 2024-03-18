0c13308084a9de305d2a20977be72a74bc38b97b
Niels van 't Slot <niels@kodea.nl>
2024-03-18 14:24:01 +0100

    slash command registration

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
-const environment = (process.env.NODE_ENV || 'development').trim();
-const envFile = environment === 'production' ? '.env.prod' : '.env';
+const environment = (process.env.NODE_ENV || "development").trim();
+const envFile = environment === "production" ? ".env.prod" : ".env";
 
 dotenv.config({ path: envFile });
 dotenv.config();
@@ -13,4 +13,6 @@ export const refreshRate = parseInt(process.env.REFRESH_RATE + "000");
 export const roleId = process.env.ROLE_ID;
 export const graphLength = parseInt(process.env.GRAPH_LENGTH);
 export const url = process.env.URL;
+export const clientId = process.env.CLIENT_ID;
+export const guildId = process.env.GUILD_ID;
 export const discordToken = process.env.DISCORD_TOKEN;
